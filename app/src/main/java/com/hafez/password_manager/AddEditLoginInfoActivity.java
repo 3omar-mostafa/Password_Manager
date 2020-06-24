030528b23c21bdea366cedd42376192cb64e9b21
Omar Mostafa <3omar.7afez2022@gmail.com>
2020-06-24 05:39:34 +0200

    Added Saving To Database in AddEditLoginInfoActivity

--- app/src/main/java/com/hafez/password_manager/AddEditLoginInfoActivity.java
+++ app/src/main/java/com/hafez/password_manager/AddEditLoginInfoActivity.java
@@ -7,11 +7,17 @@ import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
 import com.hafez.password_manager.databinding.ActivityAddEditLoginInfoBinding;
+import com.hafez.password_manager.models.LoginInfo;
+import com.hafez.password_manager.view_models.AddEditLoginInfoViewModel;
 
 public class AddEditLoginInfoActivity extends AppCompatActivity {
 
+    private AddEditLoginInfoViewModel viewModel;
     private ActivityAddEditLoginInfoBinding viewBinding;
 
     @Override
@@ -22,12 +28,10 @@ public class AddEditLoginInfoActivity extends AppCompatActivity {
 
         setContentView(viewBinding.getRoot());
 
-        initializeViews();
+        viewModel = new ViewModelProvider(this, new AddEditLoginInfoViewModel.Factory())
+                .get(AddEditLoginInfoViewModel.class);
 
-        viewBinding.save.setOnClickListener(v -> {
-            //TODO: Actually Save Before Closing
-            finish();
-        });
+        initializeViews();
 
     }
 
@@ -44,6 +48,29 @@ public class AddEditLoginInfoActivity extends AppCompatActivity {
         viewBinding.username.setOnFocusChangeListener(focusChangeListener);
         viewBinding.password.setOnFocusChangeListener(focusChangeListener);
 
+        viewBinding.save.setOnClickListener(v -> {
+            if (isInputValid()) {
+                saveLoginInfo();
+                finish();
+            } else {
+                showSavingErrors();
+            }
+        });
+
+    }
+
+    private boolean isInputValid() {
+        return isValidTextInput(viewBinding.username) && isValidTextInput(viewBinding.password);
+    }
+
+    private void showSavingErrors() {
+        Snackbar.make(viewBinding.save, R.string.error_save_validation, Snackbar.LENGTH_SHORT)
+                .setAnchorView(viewBinding.save)
+                .setAnimationMode(BaseTransientBottomBar.ANIMATION_MODE_FADE)
+                .show();
+
+        ifInvalidShowErrorElseRemoveError(viewBinding.username);
+        ifInvalidShowErrorElseRemoveError(viewBinding.password);
     }
 
     private void ifInvalidShowErrorElseRemoveError(@NonNull EditText editText) {
@@ -56,6 +83,14 @@ public class AddEditLoginInfoActivity extends AppCompatActivity {
         } else {
             showErrorMessage(editText);
         }
+    }
+
+    private void saveLoginInfo() {
+        String username = viewBinding.username.getText().toString();
+        String password = viewBinding.password.getText().toString();
+
+        LoginInfo loginInfo = new LoginInfo(username, password, R.drawable.ic_launcher);
+        viewModel.insertOrUpdateLoginInfo(loginInfo);
     }
 
     private boolean isValidTextInput(@NonNull EditText editText) {
